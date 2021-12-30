d17bd949919c9ddd74fa42ea49ee9a48f930d1b9
omarabbasi <omarkabbasi@outlook.com>
2021-12-30 11:13:51 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const cards = ["Happy Birthday", "Get well soon", "Happy Anniversary"];
+
+function writeCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+        console.log(`Hey Jim, ${cards[i]} love, Omar!`);
+        debugger;
+    }
+
+    return cards;
+}
+
+writeCards(cards);
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
